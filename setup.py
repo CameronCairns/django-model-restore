147532bfba7d0c65e68a09e2025292e0b80ef4b8
Cameron Cairns <code@cameroncairns.com>
2019-06-11 18:46:17 -0700

    Initializes package with license and description

---
+++ setup.py
@@ -0,0 +1,69 @@
+import setuptools
+
+VERSION = "19.1.0"
+TITLE = "django-model-restore"
+DESCRIPTION = "A more consistent Django model soft delete."
+URL = "https://www.cameroncairns.com/"
+DOC = DESCRIPTION + " <" + URL + ">"
+AUTHOR = "Cameron Cairns"
+AUTHOR_EMAIL = "cameron@cameroncairns.com"
+LICENSE = "Apache License 2.0"
+COPYRIGHT = "Copyright (c) 2019 Cameron Cairns"
+NAME = "django-model-restore"
+
+
+INSTALL_REQUIRES = ["django>=1.11,!=2.0.*"]
+PYTHON_REQUIRES = ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*"
+
+KEYWORDS = ["django", "soft-delete", "models"]
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache Software License",
+    "Framework :: Django",
+    "Framework :: Django :: 1.11",
+    "Framework :: Django :: 2.1",
+    "Framework :: Django :: 2.2",
+    "Environment :: Web Environment",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+def get_description():
+    with open("README.rst", "r") as readme:
+        long_description = readme.read()
+    return long_description
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        zip_safe=False,
+        long_description=get_description(),
+        long_description_content_type="text/x-rst",
+        packages=setuptools.find_packages(where="src"),
+        package_dir={"": "src"},
+        name=NAME,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=AUTHOR,
+        maintainer_email=AUTHOR_EMAIL,
+        keywords=KEYWORDS,
+        python_requires=PYTHON_REQUIRES,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+    )
---
+++ src/django_model_restore/__init__.py
@@ -0,0 +1,41 @@
+"""
+Django Model Restore: Soft Delete without surprises
+
+This project aims to implement soft delete in Django without some of the
+surprises that come with other soft delete libraries. Namely it will
+not return soft deleted records from the db when querying on models
+related to the model of the soft deleted instance.
+
+Example:
+
+from django.db import models
+
+class Owner(soft_delete_lib.Deletable):
+    name = models.TextField()
+
+class Poll(soft_delete_lib.Deletable):
+    name = models.TextField()
+    owner = models.ForeignKey(Owner)
+
+def test_soft_delete():
+    owner = Owner.objects.create(name='test')
+    poll = Poll.objects.create(name='test', owner=owner)
+
+    assert owner.polls.count() == 1
+
+    poll.delete()
+
+    # works with all soft delete libraries researched
+    assert owner.polls.count() == 0
+
+    # this is usually where soft delete libraries fail
+    assert list(Owner.objects.values_list(poll__name=True, flat=True)) == [None]
+
+What's happening here? Most soft delete libraries implement soft delete via a
+boolean or date flag (e.g. deleted/deleted_at). They reference this date flag
+to determine whether to return the result in the base_queryset for the manager
+of the soft deletable model.  What they are not able to do, is have queries
+on the same SQL table via other django models respect the filter on the deleted
+field. That is the issue this library aims to solve
+"""
+__package_name__ = "django-model-restore"
